1f0f16a68d0df673f894c3104efb27a96921d8ff
jessicalyruiz <jessicalyr@gmail.com>
2023-06-08 19:42:57 -0500

    Taller 14 vue, directivas

--- js/app.js
+++ js/app.js
@@ -4,10 +4,29 @@ console.log(Vue)
 
 const app=Vue.createApp({
     //opcion1
-    template:`
-    <h1> hello world </h1>
-    <p> desde vue</p>
-    `
+   // template:`
+    //<h1> hello world </h1>
+   // <p> desde vue</p>
+   // {{1+1}}
+   // `
+
+   //opcion2
+   data(){
+    return{
+        namej:'jessi',
+        apellido:'yanez',
+        mensaje: 'hello world from data, vue.js'
+    }
+   },
+   methods:{
+    cambiarName(){
+        console.log('metodo cambioar nombre')
+        this.namej='Jessicaly'
+    },
+    changeApellido(){
+        this.apellido='Ruiz'
+    }
+   }
 })
 
 app.mount('#myAppJessi')
---
+++ js/vfor.js
@@ -0,0 +1,17 @@
+console.log('vue:')
+console.log(Vue)
+
+
+const app=Vue.createApp({
+  
+   data(){
+    return{
+       
+    }
+   },
+   methods:{
+   
+   }
+})
+
+app.mount('#myAppFor')
