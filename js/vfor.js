3f9eb8b71d20ad140aec65168e7a4d5feb59602a
jessicalyruiz <jessicalyr@gmail.com>
2023-06-10 10:46:44 -0500

    taller 14 directiva v-model

--- js/vfor.js
+++ js/vfor.js
@@ -1,16 +1,93 @@
-console.log('vue:')
-console.log(Vue)
+const estudiante={
+   nombre:'Jessi',
+   apellido:'Yanez'
+}
+const estudiante2={
+   nombre:'Lore',
+   apellido:'Yanez'
+}
+const estudiante3={
+   nombre:'Taty',
+   apellido:'Yanez'
+}
+const estudiante4={
+   nombre:'Marilyn',
+   apellido:'Yanez'
+}
+const estudiante5={
+   nombre:'Jessi',
+   apellido:'Ruiz'
+}
+const estudiante6={
+   nombre:'Nancy',
+   apellido:'Ruiz'
+}
+const estudiante7={
+   nombre:'Pedro',
+   apellido:'Lin'
+}
+const estudiante8={
+   nombre:'Andres',
+   apellido:'Viv'
+}
+const estudiante9={
+   nombre:'dome',
+   apellido:'Rodriguez'
+}
+const estudiante10={
+   nombre:'joss',
+   apellido:'monca'
+}
+
+const estudiantes=[estudiante, estudiante2, estudiante3, estudiante4, estudiante5, estudiante6, estudiante7, estudiante8, estudiante9, estudiante10]
+
+console.log('estudiantes'+ estudiantes)
 
 
 const app=Vue.createApp({
   
    data(){
     return{
-       
+       arreglo:estudiantes,
+       nombre:'',
+       apellido:'',
+      
     }
    },
    methods:{
-   
+   imprimir(){
+      console.log(estudiantes)
+   },
+   ingresarEstu( event){
+         console.log('vamos a agregar: ')
+         
+         console.log(this.nombre)
+         this.estudiantes
+
+
+         console.log(event)
+         console.log(event.charCode)
+         if(event.charCode=='13'){
+            console.log('presiono enter')
+            const newStudent={
+               nombre:this.nombre,
+               apellido:'new apellido'
+            }
+            this.arreglo.push(newStudent)
+         }else {console.log('no enter')}
+
+   },
+   ingresarEstu( event){
+      console.log('vamos a agregar: ')
+      console.log(this.nombre)
+         const newStudent={
+            nombre:this.nombre,
+            apellido:'new apellido'
+         }
+         this.arreglo.push(newStudent)
+     
+
+}
    }
 })
 
